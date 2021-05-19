2c6328afdc8d010f4ed22e667c500b887138448a
Joseph Rodrigues <joseph@pc-sg.com>
2021-05-19 07:34:58 -0400

    decided to end this project as it will not complete fully IAP example

--- RazeFaces-Starter/RazeFaces/IAPHelper.swift
+++ RazeFaces-Starter/RazeFaces/IAPHelper.swift
@@ -31,12 +31,21 @@ import StoreKit
 public typealias ProductIdentifier = String
 public typealias ProductsRequestCompletionHandler = (_ success: Bool, _ products: [SKProduct]?) -> Void
 
+
+
 extension Notification.Name {
   static let IAPHelperPurchaseNotification = Notification.Name("IAPHelperPurchaseNotification")
 }
 
 open class IAPHelper: NSObject  {
+  private let productIdentifiers: Set<ProductIdentifier>
+  private var purchasedProductIdentifiers: Set<ProductIdentifier> = []
+  private var productsRequest: SKProductsRequest?
+  private var productsRequestCompletionHandler: ProductsRequestCompletionHandler?
+  
     public init(productIds: Set<ProductIdentifier>) {
+    productIdentifiers = productIds
+
     super.init()
   }
 }
@@ -45,9 +54,15 @@ open class IAPHelper: NSObject  {
 
 extension IAPHelper {
   
-  public func requestProducts(_ completionHandler: @escaping ProductsRequestCompletionHandler) {
-    completionHandler(false, [])
+  public func requestProducts(completionHandler: @escaping ProductsRequestCompletionHandler) {
+    productsRequest?.cancel()
+    productsRequestCompletionHandler = completionHandler
+
+    productsRequest = SKProductsRequest(productIdentifiers: productIdentifiers)
+    productsRequest!.delegate = self
+    productsRequest!.start()
   }
+
 
   public func buyProduct(_ product: SKProduct) {
   }
@@ -63,3 +78,32 @@ extension IAPHelper {
   public func restorePurchases() {
   }
 }
+
+// MARK: - SKProductsRequestDelegate
+
+extension IAPHelper: SKProductsRequestDelegate {
+
+  public func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
+    print("Loaded list of products...")
+    let products = response.products
+    productsRequestCompletionHandler?(true, products)
+    clearRequestAndHandler()
+
+    for p in products {
+      print("Found product: \(p.productIdentifier) \(p.localizedTitle) \(p.price.floatValue)")
+    }
+  }
+  
+  public func request(_ request: SKRequest, didFailWithError error: Error) {
+    print("Failed to load list of products.")
+    print("Error: \(error.localizedDescription)")
+    productsRequestCompletionHandler?(false, nil)
+    clearRequestAndHandler()
+  }
+
+  private func clearRequestAndHandler() {
+    productsRequest = nil
+    productsRequestCompletionHandler = nil
+  }
+}
+
